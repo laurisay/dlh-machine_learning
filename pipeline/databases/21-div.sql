24efe35cd8d40b8d075f500b291c2c4cd8f86b12
laurisay <assamsayah5@gmail.com>
2026-06-01 10:45:00 +0000

    21-div.sql

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,15 @@
+-- Creates a function SafeDiv that divides a by b or returns 0 if b = 0
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END $$
+
+DELIMITER ;
