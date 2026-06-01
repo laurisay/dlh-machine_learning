2c6ea1f8b71949a6d4f7965141b3b351b4e4d31b
laurisay <assamsayah5@gmail.com>
2026-06-01 10:22:00 +0000

    corre ction

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,3 +1,6 @@
--- INSERT VALUE to  table
--- insert id name,  rows to table
-SELECT COUNT(id) FROM first_table WHERE id=89;
+-- Lists all genres and the number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
