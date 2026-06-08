43014faa6236d6bccbdb492f82afbaabdca1cbc3
laurisay <assamsayah5@gmail.com>
2026-06-08 19:45:19 +0000

    10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module to get the shape of a numpy.ndarray."""
+
+
+def np_shape(matrix):
+    """Return the shape of a numpy.ndarray as a tuple."""
+    return matrix.shape
