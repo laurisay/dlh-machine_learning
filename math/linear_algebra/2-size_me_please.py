8e5948cb784cf3c18cbdae2e72c68e67707fe698
laurisay <assamsayah5@gmail.com>
2026-06-08 18:42:39 +0000

    2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    shape = []
+    current = matrix
+    while type(current) == list:
+        shape.append(len(current))
+        current = current[0] if len(current) > 0 else None
+    return shape
