984847cc7a94a857eb54fd30a3f989c18a89e5d7
laurisay <assamsayah5@gmail.com>
2026-06-08 20:01:13 +0000

    14-saddle_up.py

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to multiply two numpy matrices."""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Perform matrix multiplication on two numpy arrays."""
+    return np.matmul(mat1, mat2)
