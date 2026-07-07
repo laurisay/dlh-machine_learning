a731591af66c6eb8e8fc4362cfbda577648ea7b6
laurisay <assamsayah5@gmail.com>
2026-07-07 01:13:15 +0000

    correction 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -16,11 +16,9 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    
-    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+    plt.hist(student_grades, bins=10, edgecolor='black', range=(0, 100))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
     plt.xlim(0, 100)
-    plt.xticks(np.arange(0, 101, 10))
     plt.show()
