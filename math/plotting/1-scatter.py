f1c367be073d072c4e493426b7a484ed2e25ccc7
laurisay <assamsayah5@gmail.com>
2026-07-07 00:55:25 +0000

    1-scatter.py

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+1-scatter module
+
+This module contains a function to plot a scatter plot
+of men's height vs weight.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    Plots a scatter plot of men's height vs weight.
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plot x vs y as magenta points
+    plt.scatter(x, y, color='magenta')
+    
+    # Set labels and title
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title('Men\'s Height vs Weight')
+    
+    plt.show()
