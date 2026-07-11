bbda885c06a4025782ea9a60a41df455b4823a52
laurisay <assamsayah5@gmail.com>
2026-07-11 10:56:33 +0000

    correction10 5-all_in_one.py

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
-"""
-5-all_in_one module
+"""Module that plots all previous graphs in one figure."""
 
-This module contains a function to plot all 5 previous graphs
-in one figure with a 3x2 grid layout.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def all_in_one():
-    """
-    Plots all 5 previous graphs in one figure with 3x2 grid layout.
-    """
-    # Data for all plots
+    """Plot all five graphs in one figure."""
+
     y0 = np.arange(0, 11) ** 3
 
     mean = [69, 0]
@@ -37,53 +31,54 @@ def all_in_one():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
-    # Create figure with 3x2 grid
-    fig = plt.figure(figsize=(10, 10))
-    fig.suptitle('All in One')
+    fig = plt.figure(figsize=(8, 8))
+    fig.suptitle("All in One")
 
-    # Plot 1: Line Graph (0-line)
+    # Plot 1
     ax1 = plt.subplot(3, 2, 1)
     ax1.plot(y0, color='red')
     ax1.set_xlim(0, 10)
-    ax1.set_xlabel('x', fontsize='x-small')
-    ax1.set_ylabel('y', fontsize='x-small')
-    ax1.set_title('Line Graph', fontsize='x-small')
 
-    # Plot 2: Scatter (1-scatter)
+    # Plot 2
     ax2 = plt.subplot(3, 2, 2)
-    ax2.scatter(x1, y1, color='magenta')
-    ax2.set_xlabel('Height (in)', fontsize='x-small')
-    ax2.set_ylabel('Weight (lbs)', fontsize='x-small')
-    ax2.set_title('Scatter', fontsize='x-small')
+    ax2.scatter(x1, y1, s=10, color='magenta')
+    ax2.set_title("Men's Height vs Weight", fontsize='x-small')
+    ax2.set_xlabel("Height (in)", fontsize='x-small')
+    ax2.set_ylabel("Weight (lbs)", fontsize='x-small')
+    ax2.set_xlim(55, 80)
+    ax2.set_ylim(165, 190)
 
-    # Plot 3: Change of Scale (2-change_scale)
+    # Plot 3
     ax3 = plt.subplot(3, 2, 3)
     ax3.plot(x2, y2)
-    ax3.set_xlabel('Time (years)', fontsize='x-small')
-    ax3.set_ylabel('Fraction Remaining', fontsize='x-small')
-    ax3.set_title('Change of Scale', fontsize='x-small')
-    ax3.set_yscale('log')
+    ax3.set_title("Exponential Decay of C-14", fontsize='x-small')
+    ax3.set_xlabel("Time (years)", fontsize='x-small')
+    ax3.set_ylabel("Fraction Remaining", fontsize='x-small')
+    ax3.set_yscale("log")
     ax3.set_xlim(0, 28650)
 
-    # Plot 4: Two is better than one (3-two)
+    # Plot 4
     ax4 = plt.subplot(3, 2, 4)
     ax4.plot(x3, y31, 'r--', label='C-14')
     ax4.plot(x3, y32, 'g-', label='Ra-226')
-    ax4.set_xlabel('Time (years)', fontsize='x-small')
-    ax4.set_ylabel('Fraction Remaining', fontsize='x-small')
-    ax4.set_title('Two is better than one', fontsize='x-small')
+    ax4.set_title("Exponential Decay of Radioactive Elements",
+                  fontsize='x-small')
+    ax4.set_xlabel("Time (years)", fontsize='x-small')
+    ax4.set_ylabel("Fraction Remaining", fontsize='x-small')
     ax4.set_xlim(0, 20000)
     ax4.set_ylim(0, 1)
-    ax4.legend(loc='upper right', fontsize='x-small')
+    ax4.legend(fontsize='x-small')
 
-    # Plot 5: Frequency (4-frequency) - takes 2 columns (positions 5 and 6)
-    ax5 = plt.subplot(3, 2, (5, 6))
-    ax5.hist(student_grades, bins=10, edgecolor='black')
-    ax5.set_xlabel('Grades', fontsize='x-small')
-    ax5.set_ylabel('Number of Students', fontsize='x-small')
-    ax5.set_title('Frequency', fontsize='x-small')
+    # Plot 5
+    ax5 = plt.subplot(3, 1, 3)
+    ax5.hist(student_grades,
+             bins=np.arange(0, 101, 10),
+             edgecolor='black')
+    ax5.set_title("Project A", fontsize='x-small')
+    ax5.set_xlabel("Grades", fontsize='x-small')
+    ax5.set_ylabel("Number of Students", fontsize='x-small')
     ax5.set_xlim(0, 100)
-    ax5.set_xticks(np.arange(0, 101, 10))
+    ax5.set_ylim(0, 30)
 
-    plt.tight_layout()
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
     plt.show()
