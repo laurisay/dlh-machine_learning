6fd394e09864ec14becdef8671ef6be55c054a90
laurisay <assamsayah5@gmail.com>
2026-07-21 21:30:11 +0200

    z_score

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -29,3 +29,27 @@ class Normal:
             # Calcul de l'écart type (population)
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value.
+
+        Args:
+            x (float): The x-value
+
+        Returns:
+            float: The z-score of x
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score.
+
+        Args:
+            z (float): The z-score
+
+        Returns:
+            float: The x-value of z
+        """
+        return self.mean + (z * self.stddev)        
