96f522c597d329bbd72dea221f0ad1959a583926
laurisay <assamsayah5@gmail.com>
2026-07-21 21:19:30 +0200

    PDF

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -24,3 +24,20 @@ class Exponential:
                 raise ValueError("data must contain multiple values")
             mean = sum(data) / len(data)
             self.lambtha = float(1 / mean)
+
+            def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given time period.
+
+        Args:
+            x (float): Time period
+
+        Returns:
+            float: PDF value for x
+        """
+        if x < 0:
+            return 0
+        # PDF d'une distribution exponentielle: f(x) = λ * e^(-λx)
+        e = 2.7182818285  # Approximation de e
+        pdf_value = self.lambtha * (e ** (-self.lambtha * x))
+        return pdf_value
