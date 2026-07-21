a2cf051e2d90bfddc185d70393f7b4819ee51150
laurisay <assamsayah5@gmail.com>
2026-07-21 21:23:47 +0200

    CDF

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -34,3 +34,14 @@ class Exponential:
         e = 2.7182818285
         pdf_value = self.lambtha * (e ** (-self.lambtha * x))
         return pdf_value
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given time period.
+        """
+        if x < 0:
+            return 0
+        # CDF d'une distribution exponentielle: F(x) = 1 - e^(-λx)
+        e = 2.7182818285
+        cdf_value = 1 - (e ** (-self.lambtha * x))
+        return cdf_value
