a492b3fe4785c05e423c95d57d7cb2c844dbbbdc
laurisay <assamsayah5@gmail.com>
2026-07-22 22:56:17 +0200

    cdf9

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,13 +1,49 @@
 #!/usr/bin/env python3
 """Normal distribution module."""
 
-import math
-
 
 class Normal:
     """Represents a normal distribution."""
 
-    # ... tes autres méthodes ...
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Initialize a Normal distribution.
+
+        Args:
+            data (list): List of data to estimate the distribution
+            mean (float): Mean of the distribution
+            stddev (float): Standard deviation of the distribution
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.mean = float(sum(data) / len(data))
+
+            variance = sum(
+                (x - self.mean) ** 2 for x in data
+            ) / len(data)
+
+            self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value.
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score.
+        """
+        return self.mean + (z * self.stddev)
 
     def pdf(self, x):
         """
@@ -30,9 +66,23 @@ class Normal:
         """
         Calculates the value of the CDF for a given x-value.
         """
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        z = (x - self.mean) / (
+            self.stddev * (2 ** 0.5)
+        )
+
         return 0.5 * (
-            1 + math.erf(
-                (x - self.mean) /
-                (self.stddev * math.sqrt(2))
+            1 + (2 / (pi ** 0.5)) *
+            (
+                z -
+                (z ** 3) / 3 +
+                (z ** 5) / 10 -
+                (z ** 7) / 42 +
+                (z ** 9) / 216 -
+                (z ** 11) / 1320 +
+                (z ** 13) / 9360 -
+                (z ** 15) / 75600
             )
         )
