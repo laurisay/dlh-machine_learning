20ba3e3410c7d5dee78a449a2c5fdf13aba8d7de
laurisay <assamsayah5@gmail.com>
2026-07-23 00:49:42 +0200

    PMF

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -47,7 +47,6 @@ class Binomial:
             self.n = int(n)
             self.p = float(mean / self.n)
 
-
     def pmf(self, k):
         """
         Calculates the value of the PMF for a given number of successes.
