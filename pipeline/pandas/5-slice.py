6c4c03e1a5154968188338029b2ad8869e1416b1
laurisay <assamsayah5@gmail.com>
2026-08-18 20:06:29 +0200

    5-slice.py3

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """Function to slice DataFrame selecting columns and every 60th row."""
 
-import pandas as pd
-
 
 def slice(df):
     """Extracts High, Low, Close, Volume_(BTC) columns & every 60th row.
