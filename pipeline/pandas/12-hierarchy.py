d4d6ceb1eccdd2c8e82eab384fbc0ac47671e2b7
laurisay <assamsayah5@gmail.com>
2026-08-18 20:36:19 +0200

    12-hierarchy.py

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Function to create hierarchical concatenation of two DataFrames."""
+
+import pandas as pd
+from 10-index import index
+
+
+def hierarchy(df1, df2):
+    """Concatenates two DataFrames with hierarchical MultiIndex.
+
+    Args:
+        df1: pd.DataFrame (coinbase) to concatenate.
+        df2: pd.DataFrame (bitstamp) to concatenate.
+
+    Returns:
+        pd.DataFrame: concatenated DataFrame with MultiIndex.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+
+    start = 1417411980
+    end = 1417417980
+
+    df1_filtered = df1[(df1.index >= start) & (df1.index <= end)]
+    df2_filtered = df2[(df2.index >= start) & (df2.index <= end)]
+
+    df_concat = pd.concat(
+        [df2_filtered, df1_filtered],
+        keys=['bitstamp', 'coinbase']
+    )
+
+    df_concat = df_concat.swaplevel(0, 1).sort_index(level=0)
+
+    return df_concat
