6338e54170a595da5b9f22ff792948dab62173e8
laurisay <assamsayah5@gmail.com>
2026-08-18 20:26:31 +0200

    11-concat.py

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Function to concatenate two DataFrames."""
+
+from 10-index import index
+
+
+def concat(df1, df2):
+    """Concatenates two DataFrames with specified conditions.
+
+    Args:
+        df1: pd.DataFrame (coinbase) to concatenate to.
+        df2: pd.DataFrame (bitstamp) to select rows from.
+
+    Returns:
+        pd.DataFrame: concatenated DataFrame with keys.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2_filtered = df2[df2.index <= 1417411920]
+
+    return pd.concat([df2_filtered, df1], keys=['bitstamp', 'coinbase'])
