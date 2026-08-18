a31464ff0c8e0d28faa675db86b52c3dc8ed663b
laurisay <assamsayah5@gmail.com>
2026-08-18 20:34:06 +0200

    11-concat.py2

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Function to concatenate two DataFrames."""
 
-from 10-index import index
+import pandas as pd
+
+index = __import__('10-index').index
 
 
 def concat(df1, df2):
