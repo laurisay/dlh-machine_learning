90247582996fdb283333474b0afc944a1eb93eda
laurisay <assamsayah5@gmail.com>
2026-08-18 20:20:04 +0200

    9-fill.py

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -12,14 +12,14 @@ def fill(df):
         pd.DataFrame: modified DataFrame with filled values.
     """
     df = df.drop(columns=['Weighted_Price'])
-    
+
     df['Close'] = df['Close'].fillna(method='ffill')
-    
+
     df['High'] = df['High'].fillna(df['Close'])
     df['Low'] = df['Low'].fillna(df['Close'])
     df['Open'] = df['Open'].fillna(df['Close'])
-    
+
     df['Volume_(BTC)'] = df['Volume_(BTC)'].fillna(0)
     df['Volume_(Currency)'] = df['Volume_(Currency)'].fillna(0)
-    
+
     return df
