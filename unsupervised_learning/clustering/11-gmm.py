8c04d91abf652f21ceb342ff1d41d82f6fd21f48
laurisay <assamsayah5@gmail.com>
2026-09-08 11:10:40 +0200

    11-gmm.py

---
+++ unsupervised_learning/clustering/11-gmm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Module that calculates a GMM from a dataset using sklearn."""
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """
+    Calculate a GMM from a dataset using sklearn.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset.
+        k: The number of clusters.
+
+    Returns:
+        pi, m, S, clss, bic: pi is a numpy.ndarray of shape (k,)
+            containing the cluster priors, m is a numpy.ndarray of
+            shape (k, d) containing the centroid means, S is a
+            numpy.ndarray of shape (k, d, d) containing the
+            covariance matrices, clss is a numpy.ndarray of shape
+            (n,) containing the cluster indices for each data point,
+            and bic is the BIC value for the model.
+    """
+    model = sklearn.mixture.GaussianMixture(n_components=k)
+    model.fit(X)
+
+    pi = model.weights_
+    m = model.means_
+    S = model.covariances_
+    clss = model.predict(X)
+    bic = model.bic(X)
+
+    return pi, m, S, clss, bic
